ddf22fe6f0b8947418f492da23b52dae90aeb397
Newton-76 <76390647+Newton-76@users.noreply.github.com>
2021-05-05 09:55:18 +0200

    set up PWA (not finished yet)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js')
+    .then((reg) => console.log('Registration successfull', reg))
+    .catch((err) => console.log('Registration failed', err));
+}
