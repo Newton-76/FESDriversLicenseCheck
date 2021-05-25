6d9e2291d4312954e1b237383f546b700a9605a4
Newton-76 <76390647+Newton-76@users.noreply.github.com>
2021-05-25 16:37:47 +0200

    Scanner prototype

--- src/app.js
+++ src/app.js
@@ -3,3 +3,36 @@ if ("serviceWorker" in navigator) {
   //  .then((registration) => console.log("Registration successfull: ", registration))
   //    .catch((error) => console.log("Registration failed: ", error));
 }
+
+let scanButton = document.createElement("button");
+scanButton.innerHTML = "ClickMe!";
+scanButton.addEventListener("click", scanTag());
+document.body.appendChild(scanButton);
+
+
+/*
+ * Prototype, fully copied from https://web.dev/nfc/
+ */
+async function scanTag() {
+  if ("NDEFReader" in navigator) {
+    const ndef = new NDEFReader();
+    ndef.scan().then(() => {
+      console.log("Scan started successfully.");
+      ndef.onreadingerror = () => {
+        console.log("Cannot read data from the NFC tag. Try another one?");
+      };
+      ndef.onreading = event => {
+        const message = event.message;
+        for (const record of message.records) {
+          console.log("Record type:  " + record.recordType);
+          console.log("MIME type:    " + record.mediaType);
+          console.log("Record id:    " + record.id);
+        }
+      };
+    }).catch(error => {
+      console.log(`Error! Scan failed to start: ${error}.`);
+    });
+  } else {
+    alert("NFC is not supported.");
+  }
+}
