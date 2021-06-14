50fa748326acf9cd45718adc764c0d1c5b2c5e72
Newton-76 <76390647+Newton-76@users.noreply.github.com>
2021-06-14 11:23:00 +0200

    almost there

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 //Author: Denis Neumann, 1308358
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js")
+  navigator.serviceWorker.register("/FESDriversLicenseCheck/sw.js")
     .then((registration) => console.log("Registration successfull: ", registration))
       .catch((error) => console.log("Registration failed: ", error));
 }
