bc5aa44fa0786e762b4d79e0620f15f91584a59a
Newton-76 <76390647+Newton-76@users.noreply.github.com>
2021-07-05 16:36:00 +0200

    Adjusting the code after adjusting xampp

--- scan_screen.php
+++ scan_screen.php
@@ -6,12 +6,12 @@
   <meta name="theme-color" content="#040521">
   <title>FES Driver's License Check</title>
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <link rel="manifest" href="/FESDriversLicenseCheck/manifest.json">
-  <link rel="apple-touch-icon" href="/FESDriversLicenseCheck/img/apple-icon-180.png">
-  <link rel="icon" href="/FESDriversLicenseCheck/img/favicon.ico">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="apple-touch-icon" href="/img/apple-icon-180.png">
+  <link rel="icon" href="/img/favicon.ico">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script src="/FESDriversLicenseCheck/src/javascript_executions/scanner.js"></script>
-  <script src="/FESDriversLicenseCheck/src/javascript_executions/compare.js"></script>
+  <script src="/src/javascript_executions/scanner.js"></script>
+  <script src="/src/javascript_executions/compare.js"></script>
 </head>
 
 <body>
@@ -66,11 +66,11 @@
   </script>
 
   <div class="w3-bar w3-border" >
-    <a href="/FESDriversLicenseCheck/main_menu.html" class="w3-bar-item w3-button w3-blue"><i class="fa fa-home"></i></a>
+    <a href="/main_menu.html" class="w3-bar-item w3-button w3-blue"><i class="fa fa-home"></i></a>
     <a href="#" class="w3-bar-item w3-button"><i class="fa fa-search"></i></a>
-    <a href="/FESDriversLicenseCheck/personen.html" class="w3-bar-item w3-button"><i class="fa fa-male"></i></a>
+    <a href="/personen.html" class="w3-bar-item w3-button"><i class="fa fa-male"></i></a>
     <a href="#" class="w3-bar-item w3-button"><i class="fa fa-car"></i></a>
-    <a href="/FESDriversLicenseCheck/index.html" class="w3-bar-item w3-button"><i class="fa fa-sign-in"></i></a>
+    <a href="/index.html" class="w3-bar-item w3-button"><i class="fa fa-sign-in"></i></a>
   </div>
 </body>
 </html>
--- src/javascript_executions/app.js
+++ src/javascript_executions/app.js
@@ -1,7 +1,7 @@
 //Author: Denis Neumann, 1308358
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/FESDriversLicenseCheck/sw.js")
+  navigator.serviceWorker.register("/sw.js")
     .then((registration) => console.log("Registration successfull: ", registration))
       .catch((error) => console.log("Registration failed: ", error));
 }
